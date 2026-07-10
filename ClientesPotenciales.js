35dbb9ba1247321dd39ba73eb18d6a3eee7b827e
CuitlahaucOrtiz95 <aldair.onate@gnu.mx>
2026-07-10 16:09:23 -0600

    Add files via upload

---
+++ ClientesPotenciales.js
@@ -0,0 +1,20 @@
+// ============================================================
+// ARCHIVO GENERADO desde Clientes_potenciales.xlsx — 9/7/2026, 2:44:23 p.m.
+// NO editar a mano: usa el convertidor tras editar el Excel.
+// ============================================================
+window.CLIENTES_POTENCIALES = [
+    { nombre: "AQUAFARMS ENSENADA GNC", lat: 31.8603215495568, lon: -116.605236695254, precio: null, grupo: "x", estado: "Ensenada B.C." },
+    { nombre: "VENTA DE GNC FEMSA COATEPEC", lat: 19.4525228430035, lon: -96.9437073620414, precio: null, grupo: "GNC", estado: "Coatepec, Ver." },
+    { nombre: "VENTA DE GNC CAROLINA PERFORMANCE", lat: 20.683161640076484, lon: -100.84644758267, precio: null, grupo: "GNC", estado: "Salvatierra, Gto." },
+    { nombre: "VENTA DE GNC VISCOFAN", lat: 19.9759301417102, lon: -101.777343795111, precio: null, grupo: "GNC", estado: "Zacapu, Mich." },
+    { nombre: "Oportunidad de venta de GNC CM IRISH SA DE CV", lat: 21.1299831928625, lon: -101.728980019666, precio: null, grupo: "GNC", estado: "Leon, Guanajuato" },
+    { nombre: "Venta de GNC (Le Belier)", lat: 20.9181520666773, lon: -100.611763003333, precio: null, grupo: "GNC", estado: "Laguna Escondida, Guanajuato" },
+    { nombre: "Venta de GNC (Le Belier Oficinas)", lat: 20.6632721913179, lon: -100.463504100956, precio: null, grupo: "GNC", estado: "Querétaro, Querétaro" },
+    { nombre: "VENTA DE COMERCIALIZACION GND Acerlan Matrix Metals S.A. de C.V.", lat: 20.4209905089312, lon: -99.980570517835, precio: null, grupo: "GNC", estado: "San Juan del Río, Querétaro" },
+    { nombre: "VENTA DE GNC DERIVADOS DE LECHE LA ESMERALADA", lat: 20.915021775422403, lon: -100.768511377344, precio: null, grupo: "GNC", estado: "San Miguel de Allende, Gto." },
+    { nombre: "GNC Xin Point México S. de R.L. de C.V.", lat: 21.3947470076794, lon: -101.88276203315, precio: null, grupo: "GNC", estado: "Lagos de Moreno, Jal." },
+    { nombre: "VENTA DE GNC CON GOTURBINE - SEYBA PLAYA CAMPECHE (API Seybaplaya) *Planta cruz azul*", lat: 19.6543755376283, lon: -90.70456536444, precio: null, grupo: "GNC", estado: "Seybaplaya, Cam." },
+    { nombre: "VENTA DE GNC WEG MEXICO", lat: 19.8541924053925, lon: -99.2380474763179, precio: null, grupo: "GNC", estado: "Huehuetoca, Edo. Mex." },
+    { nombre: "VENTA DE GNC CONSERVAS EL MEXICANO", lat: 20.8649563135149, lon: -101.52233235776, precio: null, grupo: "GNC", estado: "Romita, Gto." },
+    { nombre: "VENTA DE GNC COFICAB PARQUE INDUSTIAL MONTERREY GOTURBINE", lat: 20.993407413949, lon: -101.473118684484, precio: null, grupo: "GNC", estado: "Monterrey, N.L." }
+  ];
